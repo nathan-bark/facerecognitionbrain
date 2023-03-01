4991c983b2f2d48ce518defb59e76c9fc5abacca
Nathan <nathanbark4@gmail.com>
2023-03-01 15:24:02 +0200

    completed basic functions and route

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,12 +1,32 @@
 import React from "react";
 import './Navigation.css'
 
-const Navigation = () => {
-    return (
+const Navigation = ({onRouteChange, isSignedIn}) => {
+    if(isSignedIn){
+        return (
         <nav className="nav">
-            <p className="f3 pa3 link dim underline pointer ">Sign Out</p>
+            <p 
+            className="f3 pa3 link dim underline pointer "
+            onClick={() => onRouteChange('signin')}
+            >Sign Out</p>
         </nav>
     );
+    } else {
+        return (
+            <nav className="nav">
+            <p 
+            className="f3 pa3 link dim underline pointer "
+            onClick={() => onRouteChange('signin')}
+            >Sign In</p>
+            <p 
+            className="f3 pa3 link dim underline pointer "
+            onClick={() => onRouteChange('register')}
+            >Register</p>
+        </nav>
+
+        )
+    }
+    
 }
 
 export default Navigation;
