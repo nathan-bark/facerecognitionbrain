925bd45cd446c77f47c007ed1acab22706953a01
Nathan <nathanbark4@gmail.com>
2023-04-08 02:07:40 +0300

    added react-hook-form validation

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,21 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const Register = ({ onRouteChange, loadUser }) => {
+  const { register, handleSubmit, watch, setValue, formState: { errors } } = useForm();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
+  const emailAddress = watch('emailAddress');
+  const password = watch('password');
+  const name = watch('name');
 
   const onEmailChange = (e) => {
-    setEmail(e.target.value)
+    const { value } = e.target;
+    setValue('emailAddress', value)
   }
 
   const onPasswordChange = (e) => {
-    setPassword(e.target.value)
+    const { value } = e.target;
+    setValue('password', value);
   }
 
   const onNameChange = (e) => {
-    setName(e.target.value)
+    const { value } = e.target;
+    setValue('name', value);
   }
 
   const onRegisterSubmit = () => {
@@ -23,7 +28,7 @@ const Register = ({ onRouteChange, loadUser }) => {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
-        email: email,
+        email: emailAddress,
         password: password,
         name: name
       })
@@ -40,7 +45,7 @@ const Register = ({ onRouteChange, loadUser }) => {
   return (
     <article className="br4 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 center shadow-2 tc">
       <main className="pa4 black-80">
-        <div className="measure">
+        <form className="measure">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f2 fw6 ph0 mh0">Register</legend>
             <div className="mt3">
@@ -50,22 +55,40 @@ const Register = ({ onRouteChange, loadUser }) => {
               <input
                 className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                 type="text"
-                name="name"
                 id="name"
                 onChange={onNameChange}
+                {...register('name', {
+                  required: 'Please enter your name!',
+                  minLength: {
+                    value: 3,
+                    message: 'Please enter a valid name!'
+                  },
+                })}
               />
+              <p className="f4 white">{errors.name?.message}</p>
             </div>
             <div className="mt3">
-              <label className="db fw6 lh-copy f5" htmlFor="email-address">
+              <label className="db fw6 lh-copy f5" htmlFor="emailAddress">
                 Email
               </label>
               <input
                 className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                 type="email"
-                name="email-address"
                 id="email-address"
-                onChange={onEmailChange}                
+                onChange={onEmailChange}  
+                {...register('emailAddress', {
+                  required: 'Please enter your email address!',
+                  minLength: {
+                    value: 4,
+                    message: 'Please enter a valid email address!'
+                  },
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: 'Please enter a valid email address!'
+                  }
+                })}              
               />
+              <p className="f4 white">{errors.emailAddress?.message}</p>
             </div>
             <div className="mv3">
               <label className="db fw6 lh-copy f5" htmlFor="password">
@@ -74,10 +97,21 @@ const Register = ({ onRouteChange, loadUser }) => {
               <input
                 className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                 type="password"
-                name="password"
                 id="password"
-                onChange={onPasswordChange}   
+                onChange={onPasswordChange}  
+                {...register('password', {
+                  required: 'Please create a password!',
+                  minLength: {
+                    value: 4,
+                    message: 'Password must be at least 4 characters'
+                  },
+                  pattern: {
+                  value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/i,
+                  message: 'Please enter a password with at least 1 letter and 1 number'
+                  }
+                })} 
               />
+              <p className="f4 white">{errors.password?.message}</p>
             </div>
           </fieldset>
           <div className="">
@@ -85,10 +119,10 @@ const Register = ({ onRouteChange, loadUser }) => {
               className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
               type="submit"
               value="Register"
-              onClick={onRegisterSubmit}
+              onClick={handleSubmit(onRegisterSubmit)}
             />
           </div>
-        </div>
+        </form>
       </main>
     </article>
   );
--- src/Components/SignInForm/SignInForm.js
+++ src/Components/SignInForm/SignInForm.js
@@ -1,24 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
+import { useForm } from 'react-hook-form';
 
 const SignInForm = ({ onRouteChange, loadUser }) => {
-  const [signInEmail, setSignInEmail] = useState("");
-  const [signInPassword, setSignInPassword] = useState("");
+  
+
+  const { register, handleSubmit, watch, setValue, formState: { errors } } = useForm()
+
+  const userEmail = watch('userEmail')
+  const userPassword = watch('userPassword');
 
   const onEmailChange = (e) => {
-    setSignInEmail(e.target.value);
-  };
+    const { value } = e.target; 
+    setValue('userEmail', value);
+    
+  }
 
   const onPasswordChange = (e) => {
-    setSignInPassword(e.target.value);
-  };
-
+    const { value } = e.target;
+    setValue('userPassword', value);
+    
+  }
   const onSignIn = () => {
     fetch('http://localhost:3000/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
-        email: signInEmail,
-        password: signInPassword
+        email: userEmail,
+        password: userPassword
       })
     })
     .then(res => res.json())
@@ -37,32 +45,49 @@ const SignInForm = ({ onRouteChange, loadUser }) => {
   return (
     <article className="br4 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 center shadow-2 tc">
       <main className="pa4 black-80">
-        <div className="measure">
+        <form className="measure" >
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
             <legend className="f2 fw6 ph0 mh0">Sign In</legend>
             <div className="mt3">
-              <label className="db fw6 lh-copy f5" htmlFor="email-address">
+              <label className="db fw6 lh-copy f5" htmlFor="userEmail">
                 Email
               </label>
               <input
                 className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                 type="email"
-                name="email-address"
+                
                 id="email-address"
                 onChange={onEmailChange}
+                {...register('userEmail', {
+                  required: 'Please enter your email!',
+                  minLength: 4,
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: 'Please enter a valid email address!'
+                  }
+                })}
               />
+              <p className="f4 white">{errors.userEmail?.message}</p>
             </div>
             <div className="mv3">
-              <label className="db fw6 lh-copy f5" htmlFor="password">
+              <label className="db fw6 lh-copy f5" htmlFor="userPassword">
                 Password
               </label>
               <input
                 className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                 type="password"
-                name="password"
+    
                 id="password"
                 onChange={onPasswordChange}
+                {...register('userPassword', {
+                  required: 'Please enter your password!',
+                  minLength: {
+                    value: 4,
+                    message: 'Please enter your password!'
+                  }
+                })}
               />
+              <p className="f4 white">{errors.userPassword?.message}</p>
             </div>
           </fieldset>
           <div>
@@ -73,7 +98,8 @@ const SignInForm = ({ onRouteChange, loadUser }) => {
               className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
               type="submit"
               value="Sign in"
-              onClick={onSignIn}
+              onClick={handleSubmit(onSignIn)}
+              
             />
           </div>
           <div className="lh-copy mt3">
@@ -85,7 +111,7 @@ const SignInForm = ({ onRouteChange, loadUser }) => {
               Register
             </p>
           </div>
-        </div>
+        </form>
       </main>
     </article>
   );
